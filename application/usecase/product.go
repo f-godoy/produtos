c4d5090cabb77b7005137020edfc7f54522e48a4
Felipe Godoy <felipe_otavio_br@hotmail.com>
2023-10-19 22:27:23 -0300

    implementa usecase

---
+++ application/usecase/product.go
@@ -0,0 +1,35 @@
+package usecase
+
+import(
+	"github.com/f-godoy/produtos-go/domain/model"
+)
+
+type ProductUseCase struct{
+	ProductRepository model.ProductRepositoryInterface
+}
+
+func (p *ProductUseCase) SaveOneProduct(name string, description string, price float64) (*model.Product, error){
+	newProduct, err := model.NewProduct(name,description,price)
+	
+	if err != nil{
+		return nil,err
+	}
+
+	productInserted, err := p.ProductRepository.CreateProduct(newProduct)
+
+	if err != nil{
+		return nil,err
+	}
+
+	return productInserted, nil
+}
+
+func (p *ProductUseCase) SearchAllProducts() (*[]model.Product, error){
+	products, err := p.ProductRepository.FetchProducts()
+
+	if err != nil{
+		return nil, err
+	}
+
+	return products, nil
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,14 @@ go 1.19
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.1.1
+	gorm.io/driver/sqlite v1.5.4
 )
 
-require github.com/jinzhu/inflection v1.0.0 // indirect
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55 // indirect
+)
